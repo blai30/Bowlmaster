22c2ec60f2ae1f2f3d9cd8512fcae83c74db67b0
Brian Lai <brianlai30@outlook.com>
2018-03-12 21:43:19 -0700

    Added method to launch ball and play sound

---
+++ Assets/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed;
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+	
+	void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    public void Launch() {
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    void Update() {
+		
+	}
+
+}
