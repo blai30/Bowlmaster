74f746943d15e9d3236ac9eb3c1c9988ae03dbd4
Brian Lai <brianlai30@outlook.com>
2018-03-14 16:56:25 -0700

    Added Pin.cs

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float standingThreshold = 5f;
+	
+	void Start() {
+		IsStanding();
+	}
+	
+	void Update() {
+		
+	}
+
+    public bool IsStanding() {
+        print(name + " " + transform.rotation.eulerAngles);
+        return true;
+    }
+
+}
