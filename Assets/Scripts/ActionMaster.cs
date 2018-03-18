02738666c64c949495443a736b2394b944294ad1
Brian Lai <brianlai30@outlook.com>
2018-03-17 17:56:43 -0700

    Added action master stuff

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+    public enum Action {Tidy, Reset, EndTurn, EndGame}
+
+    public Action Bowl(int pins) {
+        return Action.Tidy;
+    }
+
+}
