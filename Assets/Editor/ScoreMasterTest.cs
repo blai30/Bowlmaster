9704ec5bd2b99ffe29064cd4637b2e9c5656e039
Brian Lai <brianlai30@outlook.com>
2018-03-17 17:32:10 -0700

    Added ScoreMasterTest.cs

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+public class ScoreTest {
+
+	[Test]
+	public void PassingTest() {
+		Assert.AreEqual(1, 1);
+	}
+
+}
