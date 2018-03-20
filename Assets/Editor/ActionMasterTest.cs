664b16e80e9927166cc606699d3f692ea83ff38c
Brian Lai <brianlai30@outlook.com>
2018-03-19 17:12:57 -0700

    Refactored actionMaster

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -6,8 +6,14 @@ using NUnit.Framework;
 [TestFixture]
 public class ActionMasterTest {
 
+    private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
     private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+
+    [SetUp]
+    public void Setup() {
+        actionMaster = new ActionMaster();
+    }
 
 	[Test]
 	public void T00PassingTest() {
@@ -16,14 +22,17 @@ public class ActionMasterTest {
 
     [Test]
     public void T01OneStrikeReturnsEndTurn() {
-        ActionMaster actionMaster = new ActionMaster();
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
     }
 
     [Test]
     public void T02Bowl8ReturnsTidy() {
-        ActionMaster actionMaster = new ActionMaster();
         Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03Bowl28ReturnsEndTurn() {
+
     }
 
 }
