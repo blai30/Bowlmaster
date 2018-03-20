bcd83fdf6a5ba9256ff53555a12e4c046e850071
Brian Lai <brianlai30@outlook.com>
2018-03-19 18:24:53 -0700

    Added Dondi test

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -108,4 +108,16 @@ public class ActionMasterTest {
         Assert.AreEqual(endTurn, actionMaster.Bowl(1));
     }
 
+    [Test]
+    public void T11Dondi10thFrameTurkey() {
+        //             1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18
+        int[] rolls = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        foreach (int roll in rolls) {
+            actionMaster.Bowl(roll);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
 }
