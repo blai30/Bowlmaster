926bc09a40d016c314a3fa217db1db5931c4ee7b
Brian Lai <brianlai30@outlook.com>
2018-03-20 21:47:06 -0700

    Modified code in GameManager.cs and ScoreDisplay.cs

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -8,11 +8,16 @@ public class ScoreDisplay : MonoBehaviour {
     public Text[] rollTexts, frameTexts;
 
 	void Start() {
-		
+		rollTexts[0].text = "X";
+        frameTexts[0].text = "0";
 	}
 	
 	void Update() {
 		
 	}
+
+    public void FillRollCard(List<int> rolls) {
+        rolls[-1] = 1;
+    }
 
 }
