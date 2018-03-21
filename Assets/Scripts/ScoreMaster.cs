9c2ab6f05dadde58a2db634eb3828382a8b0c9e4
Brian Lai <brianlai30@outlook.com>
2018-03-20 18:41:48 -0700

    Uncommented some tests and added code to make tests successful

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -19,11 +19,30 @@ public class ScoreMaster {
 
     // Returns a list of individual frame scores, NOT cumulative
     public static List<int> ScoreFrames(List<int> rolls) {
-        List<int> frameList = new List<int>();
+        List<int> frames = new List<int>();
 
-        // Code here
+        for (int i = 1; i < rolls.Count; i += 2) {
+            if (frames.Count == 10) {                       // Prevents 11th frame score
+                break;
+            }
 
-        return frameList;
+            if (rolls[i-1] + rolls[i] < 10) {               // Normal "open" frame
+                frames.Add(rolls[i-1] + rolls[i]);
+            }
+
+            if (rolls.Count - i <= 1) {                     // Insufficient look-ahead
+                break;
+            }
+
+            if (rolls[i-1] == 10) {                         // Strike
+                i--;                                        // Strike frame has just one bowl
+                frames.Add(10 + rolls[i+1] + rolls[i+2]);
+            } else if (rolls[i-1] + rolls[i] == 10) {       // Calculate spare bonus
+                frames.Add(10 + rolls[i+1]);
+            }
+        }
+
+        return frames;
     }
 
 }
